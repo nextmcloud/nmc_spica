9af98f969731c927cecf208fce3d3f278555cc87
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-01-25 14:26:09 +0100

    raised max nc version, refactored php code

--- lib/Listener/BeforeTemplateRenderedListener.php
+++ lib/Listener/BeforeTemplateRenderedListener.php
@@ -11,13 +11,41 @@ declare(strict_types=1);
  */
 namespace OCA\NmcSpica\Listener;
 
+use OCA\NmcSpica\AppInfo\Application;
+use OCA\NmcSpica\Service\SpicaMailService;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\IConfig;
+use OCP\IInitialStateService;
 
 /** @template-implements IEventListener<Event|BeforeTemplateRenderedEvent> */
 class BeforeTemplateRenderedListener implements IEventListener {
+
+	/** @var IConfig */
+	private $config;
+
+	/** @var IInitialStateService */
+	private $initialState;
+
+	/** @var SpicaMailService */
+	private $spicaMailService;
+
+	/**
+	 * BeforeTemplateRenderedListener constructor.
+	 *
+	 * @param IInitialStateService $initialState
+	 */
+	public function __construct(
+		IConfig $config,
+		IInitialStateService $initialState,
+		SpicaMailService $spicaMailService
+	) {
+		$this->config = $config;
+		$this->initialState = $initialState;
+		$this->spicaMailService = $spicaMailService;
+	}
 
 	public function handle(Event $event): void {
 
@@ -28,6 +56,16 @@ class BeforeTemplateRenderedListener implements IEventListener {
 		if (!$event->isLoggedIn() || $event->getResponse()->getRenderAs() !== TemplateResponse::RENDER_AS_USER) {
 			return;
 		}
+
+		$this->initialState->provideLazyInitialState(Application::APP_ID, 'unread-counter', function () {
+			$unreadCounter = $this->spicaMailService->getUnreadCounter();
+			return $unreadCounter;
+		});
+
+		$this->initialState->provideLazyInitialState(Application::APP_ID, 'mail-url', function () {
+			$mailUrl = $this->config->getAppValue(Application::APP_ID, Application::APP_CONFIG_WEBMAIL_URL, '');
+			return $mailUrl;
+		});
 
 		// show email button only to logged in users
 		\OCP\Util::addScript("nmc_spica", "nmc_spica");
